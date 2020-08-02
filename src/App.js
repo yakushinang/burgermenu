7398db3491343864705cec1c81f9111a39741a16
yakushinang <yakushin.ang@gmail.com>
2020-08-02 22:01:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import BurgerMenu from './BurgerMenu/BurgerMenu'
 
 function App() {
+  const [message, setMessage] = useState(null)
+
+  const callbackFunction = (e) => {
+      setMessage(e.target)
+      console.log("Parant GELIK ", e)
+  }
+
+  const myOpeningAllert = () =>{
+    // alert("it opening!!!!!")
+    console.log("Click to open!!!!!")
+  }
+
+  const myClosingAllert = () =>{
+    // alert("it close!!!!!")
+    console.log("Click to close!!!!!")
+  }
+
+  const myStartTtansition = ()=>{
+    console.log("entering!!!!!")
+  }
+
+  const myCloseTtansition = ()=>{
+    console.log(" Menu has entered!!!!!")
+  }
+
+  const onExiting = ()=>{
+    console.log("start close transition ")
+  }
+
+  const onExited = ()=>{
+    console.log("Menu closed ")
+  }
+
+  console.log(message)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header" >
+          <div className="menuBar">
+              
+                <BurgerMenu 
+                      // width= {'auto'}
+                      height = {'auto'}
+                      isOpen  = {false}
+                      onEnter={myOpeningAllert} 
+                      onEntering={myStartTtansition}
+                      onEntered = {myCloseTtansition}
+
+                      onExit={myClosingAllert} 
+                      onExiting = {onExiting}
+                      onExited = {onExited}
+                      onMenuItemClick = {callbackFunction}
+                      buttonBorder = {true}
+                      // transitionDuration="0.6"
+                      menuAnimation = "fallDown"
+                      // buttonBorder = "true"
+                      // menuBarTop = {125}
+                      classes={{line: "bgRed"}} 
+                      >
+                    <button dtat-href="#hi" >illustrations</button>
+                    <button data-href="#hello" >about</button>
+                </BurgerMenu>
+
+          </div>
+      </div>
     </div>
   );
 }
