bf440623066edcd3dd8f0c60fea45a247696dffd
yakushinang <yakushin.ang@gmail.com>
2020-08-05 11:52:16 +0300

    Updates

---
+++ precache-manifest.e46590e61133fca98bd67346fd34fde2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d50b8cfbd161a28e507fd33959f80c",
+    "url": "/animated-burger-menu/index.html"
+  },
+  {
+    "revision": "90ee0ee57816e45ba25a",
+    "url": "/animated-burger-menu/static/css/main.686bce94.chunk.css"
+  },
+  {
+    "revision": "33bf7546954cb202babf",
+    "url": "/animated-burger-menu/static/js/2.cd4cf0d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animated-burger-menu/static/js/2.cd4cf0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ee0ee57816e45ba25a",
+    "url": "/animated-burger-menu/static/js/main.6e00e72b.chunk.js"
+  },
+  {
+    "revision": "1c8d3d8d5e18182009be",
+    "url": "/animated-burger-menu/static/js/runtime-main.ede528ce.js"
+  },
+  {
+    "revision": "40b253b1d985d50fb8dd1fe24f120fbd",
+    "url": "/animated-burger-menu/static/media/Image005.40b253b1.jpg"
+  }
+]);
